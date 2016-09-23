09d923e8a24eed00f17e40d1740d7112b30e1cb1
lukpon <lukas@ponikowski.de>
2016-09-23 18:36:36 +0200

    added Credits

--- Down The Mountain/assets/scripts/Start.js
+++ Down The Mountain/assets/scripts/Start.js
@@ -36,10 +36,16 @@ cc.Class({
             default:null,
             type: cc.Label
         },
+
+        creditsContainer: {
+            default:null,
+            type: cc.Node
+        },
     },
 
     // use this for initialization
     onLoad: function () {
+
     },
 
     showAboutPanel:function () {
@@ -48,6 +54,7 @@ cc.Class({
         this.aboutButton.active = false;
         this.aboutPanel.active = true;
         this.closeButton.active = true;
+        this.startCredits();
     },
 
     showTutorialPanel:function () {
@@ -65,6 +72,34 @@ cc.Class({
         this.aboutButton.active = true;
         this.playButton.active = true;
         this.tutorialButton.active = true;
+    },
+
+    startCredits: function(){
+
+        var self = this;
+
+        // Abspann mit Namen
+        var i = this.creditsContainer.getPosition().y; 
+
+        function creditsRun () {
+            setTimeout(function () {
+                //console.log('y='+i);
+                i++;
+                if (i < 30) {
+                    self.creditsContainer.setPosition(0,i); 
+                    creditsRun();
+                } else {
+                    self.creditsContainer.setPosition(0,-820); 
+                }
+            }, 10)
+        }
+
+        creditsRun();
+
+        /* TODO */
+        // fehlt noch, dass der Abspann vorzeitig stoppt, 
+        // wenn man den Close-Button drückt ***KEIN PLAN***
+  
     }
 
     // called every frame, uncomment this function to activate update callback
