de7ed1c2f1e9cc7e0c1d30b31d8d77103be3ac42
lukpon <lukas@ponikowski.de>
2016-09-30 14:31:00 +0200

    build

---
+++ Down The Mountain/_BUILD/web-desktop/res/raw-assets/resources/LevelCreator/Tiles_WonkyWoods.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tiles" tilewidth="64" tileheight="64" tilecount="11" columns="0">
+ <tile id="1">
+  <image width="64" height="64" source="Tiles/isocube.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="Tiles/lava.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="Tiles/posion.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="Tiles/spike.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="Tiles/sticky.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="Tiles/trap.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="Tiles/waterplane_l.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="Tiles/waterplane_r.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="Tiles/isocube_point.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="Tiles/isocube_potion.png"/>
+ </tile>
+ <tile id="43">
+  <image width="64" height="64" source="Tiles/isocube_flip.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="Tiles/isocube_tree.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="Tiles/treejump.png"/>
+ </tile>
+  <tile id="99">
+  <image width="64" height="64" source="Tiles/leer.png"/>
+ </tile>
+</tileset>
